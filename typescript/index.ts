0f6ec93e55607e80ff95e66f8230cfbd56dc2e9f
Manik <manik@pixelconception.com>
2023-06-17 16:15:49 +0530

    added new lecture

--- typescript/index.ts
+++ typescript/index.ts
@@ -2,14 +2,30 @@
 // premium - 20
 // gold  - 30
 
-class Discount {
-  giveDiscount(customerType: "premium" | "regular"): number {
-    if (customerType === "regular") {
-      return 10;
-    } else if (customerType === "premium") {
-      return 20;
-    } else {
-      return 10;
-    }
+interface Customer {
+  giveDiscount(): number;
+}
+
+class RegularCustomer implements Customer {
+  giveDiscount(): number {
+    return 10;
   }
 }
+
+class PremiumCustomer implements Customer {
+  giveDiscount(): number {
+    return 20;
+  }
+}
+
+class Discount {
+  giveDiscount(customer: Customer): number {
+    return customer.giveDiscount();
+  }
+}
+
+let premiumCustomer: PremiumCustomer = new PremiumCustomer();
+let discount: Discount = new Discount();
+
+let finalValue = discount.giveDiscount(premiumCustomer);
+console.log(finalValue);
