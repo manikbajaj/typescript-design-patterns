2e49b72813cb98f2884c7a56dab18a47bc8738f4
Manik <manik@pixelconception.com>
2023-06-17 19:13:05 +0530

    Update index.ts

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,20 +1,39 @@
-// regular - 10
-// premium - 20
-// gold  - 30
+// regular - 10, 1
+// premium - 20, 2
+// gold  - 30, 3
 
 interface Customer {
   giveDiscount(): number;
+  addLoyaltyPoints(amountSpent: number): number;
 }
 
 class RegularCustomer implements Customer {
   giveDiscount(): number {
     return 10;
   }
+
+  addLoyaltyPoints(amountSpent: number): number {
+    return amountSpent;
+  }
 }
 
 class PremiumCustomer implements Customer {
   giveDiscount(): number {
     return 20;
+  }
+
+  addLoyaltyPoints(amountSpent: number): number {
+    return amountSpent * 2;
+  }
+}
+
+class GoldCustomer implements Customer {
+  giveDiscount(): number {
+    return 30;
+  }
+
+  addLoyaltyPoints(amountSpent: number): number {
+    return amountSpent * 3;
   }
 }
 
@@ -25,7 +44,8 @@ class Discount {
 }
 
 let premiumCustomer: PremiumCustomer = new PremiumCustomer();
+let goldCustomer: GoldCustomer = new GoldCustomer();
 let discount: Discount = new Discount();
 
-let finalValue = discount.giveDiscount(premiumCustomer);
+let finalValue = discount.giveDiscount(goldCustomer);
 console.log(finalValue);
