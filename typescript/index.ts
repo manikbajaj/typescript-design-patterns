38d5726f26e1f20d442babfc272dcca0207e5ec7
Manik <manik@pixelconception.com>
2023-06-17 13:22:22 +0530

    Update lecture

---
+++ typescript/index.ts
@@ -0,0 +1,11 @@
+class User {
+  constructor(name: string, email: string) {}
+}
+
+class UserAuthentication {
+  constructor(user: User) {}
+
+  authenticate(password: string) {
+    // implementation logic ehre
+  }
+}
