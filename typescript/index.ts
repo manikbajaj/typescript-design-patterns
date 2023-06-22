fed6b60d2c31773cb6100ee9011660b46388cac2
Manik <manik@pixelconception.com>
2023-06-22 13:26:12 +0530

    Update index.ts

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,47 +1,31 @@
-// Payment Processor
-// Credit Card
-// Debit Card
-// PayPal
-
-abstract class PaymentProcessor {
-  abstract processPayment(amount: number): void;
+interface Printer {
+  print(document: Document): void;
 }
 
-class CreditCardProcessor extends PaymentProcessor {
-  processPayment(amount: number): void {
-    console.log(`Processing Credit Card Payments - Amount $${amount}`);
+interface Scanner {
+  scan(document: Document): void;
+}
+
+interface FaxMachine {
+  fax(document: Document): void;
+}
+
+class SimplePrinter implements Printer {
+  print(document: Document): void {
+    console.log("The Machine is printing");
   }
 }
 
-class DebitCardProcessor extends PaymentProcessor {
-  processPayment(amount: number): void {
-    console.log(`Processing Debit Card Payments - Amount $${amount}`);
+class MultiFunctionPrinter implements Printer, Scanner, FaxMachine {
+  print(document: Document): void {
+    console.log("The Machine is printing");
+  }
+
+  scan(document: Document): void {
+    console.log("The Machine is scanning");
+  }
+
+  fax(document: Document): void {
+    console.log("The Machine is sedning a fax");
   }
 }
-
-class PayPalProcessor extends PaymentProcessor {
-  processPayment(amount: number): void {
-    console.log(`Processing PayPal Payments - Amount $${amount}`);
-  }
-}
-
-class BitcoinProcessor extends PaymentProcessor {
-  processPayment(amount: number): void {
-    console.log(`Processing Bitcoin Payments - Amount $${amount}`);
-  }
-}
-
-function executePayment(
-  paymentProcessor: PaymentProcessor,
-  amount: number
-): void {
-  paymentProcessor.processPayment(amount);
-}
-
-let creditCardProcessor = new CreditCardProcessor();
-let debitCardProcessor = new DebitCardProcessor();
-let payPalProcessor = new PayPalProcessor();
-
-executePayment(creditCardProcessor, 100);
-executePayment(debitCardProcessor, 50);
-executePayment(payPalProcessor, 150);
