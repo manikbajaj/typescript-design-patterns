20466c14f5a0aeeab8be583422c17efac1036a89
Manik <manik@pixelconception.com>
2023-06-22 13:44:58 +0530

    Update index.ts

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,31 +1,51 @@
-interface Printer {
-  print(document: Document): void;
+// creating posts
+// commenting posts
+// sharing posts
+// Admin  - 3
+// Regular -2
+
+interface Post {
+  title: string;
+  content: string;
 }
 
-interface Scanner {
-  scan(document: Document): void;
+interface Comment {
+  title: string;
+  content: string;
 }
 
-interface FaxMachine {
-  fax(document: Document): void;
+interface PostCreator {
+  createPost(post: Post): void;
 }
 
-class SimplePrinter implements Printer {
-  print(document: Document): void {
-    console.log("The Machine is printing");
+interface CommentCreator {
+  createComment(comment: Comment): void;
+}
+
+interface PostSharer {
+  sharePosts(post: Post): void;
+}
+
+class Admin implements PostCreator, CommentCreator, PostSharer {
+  createPost(post: Post): void {
+    console.log("Admin is creating  a post");
+  }
+
+  createComment(comment: Comment): void {
+    console.log("Admin is creating  a comment");
+  }
+
+  sharePosts(post: Post): void {
+    console.log("Admin is sharing a post");
   }
 }
 
-class MultiFunctionPrinter implements Printer, Scanner, FaxMachine {
-  print(document: Document): void {
-    console.log("The Machine is printing");
+class RegularUser implements CommentCreator, PostSharer {
+  createComment(comment: Comment): void {
+    console.log("Regular User is creating  a comment");
   }
 
-  scan(document: Document): void {
-    console.log("The Machine is scanning");
-  }
-
-  fax(document: Document): void {
-    console.log("The Machine is sedning a fax");
+  sharePosts(post: Post): void {
+    console.log("Regular User is sharing a post");
   }
 }
