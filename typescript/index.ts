fd031f4741f0be69098c8ef914a1f55cbcc79386
Manik <manik@pixelconception.com>
2023-06-22 11:35:12 +0530

    Update index.ts

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,16 +1,41 @@
-class Bird {
-  fly(): void {
-    console.log("Bird is flying");
+// Payment Processor
+// Credit Card
+// Debit Card
+// PayPal
+
+abstract class PaymentProcessor {
+  abstract processPayment(amount: number): void;
+}
+
+class CreditCardProcessor extends PaymentProcessor {
+  processPayment(amount: number): void {
+    console.log(`Processing Credit Card Payments - Amount $${amount}`);
   }
 }
 
-class FlightlessBird extends Bird {
-  fly(): void {
-    console.log("Flightless Birds Cannot Fly");
+class DebitCardProcessor extends PaymentProcessor {
+  processPayment(amount: number): void {
+    console.log(`Processing Debit Card Payments - Amount $${amount}`);
   }
 }
 
-class Penguin extends FlightlessBird {}
+class PayPalProcessor extends PaymentProcessor {
+  processPayment(amount: number): void {
+    console.log(`Processing PayPal Payments - Amount $${amount}`);
+  }
+}
 
-let penguin: Penguin = new Penguin();
-console.log(penguin.fly());
+function executePayment(
+  paymentProcessor: PaymentProcessor,
+  amount: number
+): void {
+  paymentProcessor.processPayment(amount);
+}
+
+let creditCardProcessor = new CreditCardProcessor();
+let debitCardProcessor = new DebitCardProcessor();
+let payPalProcessor = new PayPalProcessor();
+
+executePayment(creditCardProcessor, 100);
+executePayment(debitCardProcessor, 50);
+executePayment(payPalProcessor, 150);
