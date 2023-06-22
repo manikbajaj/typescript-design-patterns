6ddd02ff5e2c54db8cec33b58e42e86d16db998f
Manik <manik@pixelconception.com>
2023-06-22 15:38:15 +0530

    Update index.ts

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,51 +1,11 @@
-// creating posts
-// commenting posts
-// sharing posts
-// Admin  - 3
-// Regular -2
-
-interface Post {
-  title: string;
-  content: string;
+class MySqlDatabase {
+  save(data: string): void {}
 }
 
-interface Comment {
-  title: string;
-  content: string;
-}
+class HighLevelModule {
+  constructor(private database: MySqlDatabase) {}
 
-interface PostCreator {
-  createPost(post: Post): void;
-}
-
-interface CommentCreator {
-  createComment(comment: Comment): void;
-}
-
-interface PostSharer {
-  sharePosts(post: Post): void;
-}
-
-class Admin implements PostCreator, CommentCreator, PostSharer {
-  createPost(post: Post): void {
-    console.log("Admin is creating  a post");
-  }
-
-  createComment(comment: Comment): void {
-    console.log("Admin is creating  a comment");
-  }
-
-  sharePosts(post: Post): void {
-    console.log("Admin is sharing a post");
-  }
-}
-
-class RegularUser implements CommentCreator, PostSharer {
-  createComment(comment: Comment): void {
-    console.log("Regular User is creating  a comment");
-  }
-
-  sharePosts(post: Post): void {
-    console.log("Regular User is sharing a post");
+  execute(data: string) {
+    this.database.save(data);
   }
 }
