5f0682490fd0c836da42eda511b4357ffbf381b8
Manik <manik@pixelconception.com>
2023-06-23 11:36:37 +0530

    Update index.ts

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,11 +1,32 @@
-class MySqlDatabase {
-  save(data: string): void {}
+interface IDatabase {
+  save(data: string): void;
+}
+
+class MySqlDatabase implements IDatabase {
+  save(data: string): void {
+    console.log(`${data} is being saved to MySQL`);
+  }
+}
+
+class MongoDbDatabase implements IDatabase {
+  save(data: string): void {
+    console.log(`${data} is being saved to MongoDB`);
+  }
 }
 
 class HighLevelModule {
-  constructor(private database: MySqlDatabase) {}
+  constructor(private database: IDatabase) {}
 
   execute(data: string) {
     this.database.save(data);
   }
 }
+
+let mysql: MySqlDatabase = new MySqlDatabase();
+let mongo: MongoDbDatabase = new MongoDbDatabase();
+
+let user: HighLevelModule = new HighLevelModule(mysql);
+user.execute("John");
+
+let post: HighLevelModule = new HighLevelModule(mongo);
+post.execute("New Post");
