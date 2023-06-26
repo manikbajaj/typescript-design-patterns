3a0dbe7d9a35fbec14275f05a721ca4bb602f1fd
Manik <manik@pixelconception.com>
2023-06-26 13:08:59 +0530

    Update index.ts

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,16 +1,59 @@
-class Animal {
-  constructor(public name: string) {}
+class Product {
+  constructor(
+    public id: string,
+    public price: number,
+    public description: string
+  ) {}
 
-  move(distance: number): void {
-    console.log(`${this.name} moved ${distance} meters.`);
+  display(): void {
+    console.log(
+      `ID: ${this.id}, Price: ${this.price}, Description: ${this.description} `
+    );
   }
 }
 
-class Dog extends Animal {
-  constructor(public name: string = "dog") {
-    super(name);
+class Book extends Product {
+  constructor(
+    public id: string,
+    public price: number,
+    public description: string,
+    public title: string,
+    public author: string
+  ) {
+    super(id, price, description);
+  }
+
+  display(): void {
+    super.display();
+    console.log(`Author: ${this.author}, Title: ${this.title}`);
   }
 }
 
-let myDog = new Dog("Max");
-myDog.move(5);
+class Electronic extends Product {
+  constructor(
+    public id: string,
+    public price: number,
+    public description: string,
+    public brand: string,
+    public model: string
+  ) {
+    super(id, price, description);
+  }
+
+  display(): void {
+    super.display();
+    console.log(`Brand: ${this.brand}, Model: ${this.model}`);
+  }
+}
+
+let book = new Book("1", 19.99, "A good book", "John Doe", "John Doe's Book");
+book.display();
+
+let laptop = new Electronic(
+  "2",
+  2999,
+  "This is a good Laptop",
+  "Dell",
+  "XPS 15"
+);
+laptop.display();
