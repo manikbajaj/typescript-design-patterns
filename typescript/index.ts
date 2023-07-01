ce7e1054dc285721f5898dd6622b906b4bb26479
Manik <manik@pixelconception.com>
2023-07-01 15:49:28 +0530

    Update index.ts

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,50 +1,97 @@
-interface IProductA {
-  operationA(): string;
+interface Button {
+  render(): void;
+  onClick(f: Function): void;
 }
 
-interface IProductB {
-  operationB(): string;
-  combinedOperation(collaborator: IProductA): string;
+interface Checkbox {
+  render(): void;
+  toggle(): void;
 }
 
-interface IFactory {
-  createProductA(): IProductA;
-  createProductB(): IProductB;
+interface GUIFactory {
+  createButton(): Button;
+  createCheckbox(button: Button): Checkbox;
 }
 
-class ProductA implements IProductA {
-  public operationA(): string {
-    return "This is the result of Operation A";
+class WindowsButton implements Button {
+  public render(): void {
+    console.log("Render a button in Windows Style");
+  }
+  public onClick(f: Function): void {
+    console.log("Windows button was clicked");
+    f();
   }
 }
 
-class ProductB implements IProductB {
-  public operationB(): string {
-    return "This is the result of Operation B";
+class WindowsCheckbox implements Checkbox {
+  constructor(private button: Button) {}
+
+  public render(): void {
+    console.log("Render a checkbox in Windows Style");
   }
 
-  public combinedOperation(collaborator: IProductA): string {
-    const result = collaborator.operationA();
-    return `The result of Product B Collobrating with (${result})`;
-  }
-}
-
-class Factory implements IFactory {
-  public createProductA(): IProductA {
-    return new ProductA();
-  }
-
-  public createProductB(): IProductB {
-    return new ProductB();
+  toggle(): void {
+    this.button.onClick(() => {
+      console.log("Windows checkbox toggled");
+    });
   }
 }
 
-//  Client Code
-const factory = new Factory();
+class MacOSButton implements Button {
+  public render(): void {
+    console.log("Render a button in MacOS Style");
+  }
+  public onClick(f: Function): void {
+    console.log("MacOS button was clicked");
+    f();
+  }
+}
 
-const productA = factory.createProductA();
-console.log(productA.operationA());
+class MacOSCheckbox implements Checkbox {
+  constructor(private button: Button) {}
 
-const productB = factory.createProductB();
-console.log(productB.combinedOperation(productA));
-console.log(productB.operationB());
+  public render(): void {
+    console.log("Render a checkbox in MacOS Style");
+  }
+
+  toggle(): void {
+    this.button.onClick(() => {
+      console.log("MacOS checkbox toggled");
+    });
+  }
+}
+
+class WindowsFactory implements GUIFactory {
+  public createButton(): Button {
+    return new WindowsButton();
+  }
+  public createCheckbox(button: Button): Checkbox {
+    return new WindowsCheckbox(button);
+  }
+}
+
+class MacOsFactory implements GUIFactory {
+  public createButton(): Button {
+    return new MacOSButton();
+  }
+  public createCheckbox(button: Button): Checkbox {
+    return new MacOSCheckbox(button);
+  }
+}
+
+// Client Code
+function renderUI(factory: GUIFactory) {
+  const button = factory.createButton();
+  const checkbox = factory.createCheckbox(button);
+
+  button.render();
+  checkbox.render();
+
+  button.onClick(() => {
+    console.log("Button Clicked");
+  });
+  checkbox.toggle();
+}
+
+renderUI(new WindowsFactory());
+renderUI(new MacOsFactory());
