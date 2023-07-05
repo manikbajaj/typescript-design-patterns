ce0616c3bf6ef75803fd63dcba429949cf9ee5f4
Manik <manik@pixelconception.com>
2023-07-05 13:56:05 +0530

    Update index.ts

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,40 +1,70 @@
-class Grinder {
-  public grindBeads(): void {
-    console.log("Grinding beans ...");
+class Amplifier {
+  public turnOn(): void {
+    console.log("Amplifier is turned On");
+  }
+
+  public setVolume(level: number): void {
+    console.log(`Volume is set to ${level}`);
   }
 }
 
-class Boiler {
-  public boilWater(): void {
-    console.log("Boiling Water ...");
+class DvdPlayer {
+  public turnOn(): void {
+    console.log("The DVD Player is tuned on");
+  }
+
+  public play(movie: string) {
+    console.log(`Playing ${movie}`);
   }
 }
 
-class Brewer {
-  public brewCoffee(): void {
-    console.log("Brewing Coffee ...");
+class Projector {
+  public turnOn(): void {
+    console.log("Projector is turned on");
+  }
+
+  public setInput(dvdPlayer: DvdPlayer): void {
+    console.log("Input set to DvdPlayer");
   }
 }
 
-class CoffeeMakerFacade {
+class Lights {
+  public dim(level: number): void {
+    console.log(`Lights dimmed to ${level}`);
+  }
+}
+
+class HomeTheaterFacade {
   constructor(
-    private grinder: Grinder,
-    private boiler: Boiler,
-    private brewer: Brewer
+    private amplifier: Amplifier,
+    private dvdPlayer: DvdPlayer,
+    private projector: Projector,
+    private lights: Lights
   ) {}
 
-  public makeCoffee() {
-    this.grinder.grindBeads();
-    this.boiler.boilWater();
-    this.brewer.brewCoffee();
-    console.log("The Coffee is ready");
+  public watchMovie(movie: string, volume: number, level: number) {
+    console.log(`Get Ready To Watch ${movie}`);
+    this.lights.dim(level);
+    this.amplifier.turnOn();
+    this.amplifier.setVolume(volume);
+    this.dvdPlayer.turnOn();
+    this.projector.turnOn();
+    this.projector.setInput(this.dvdPlayer);
+    this.dvdPlayer.play(movie);
   }
 }
 
-// Client code
-let grinder = new Grinder();
-let boilder = new Boiler();
-let brewer = new Brewer();
+// Client Code
+let amplifier = new Amplifier();
+let dvdPlayer = new DvdPlayer();
+let projector = new Projector();
+let lights = new Lights();
 
-let coffeeMaker = new CoffeeMakerFacade(grinder, boilder, brewer);
-coffeeMaker.makeCoffee();
+let homeTheater = new HomeTheaterFacade(
+  amplifier,
+  dvdPlayer,
+  projector,
+  lights
+);
+
+homeTheater.watchMovie("Inception", 10, 5);
