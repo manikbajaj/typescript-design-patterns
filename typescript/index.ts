18f3076f9e645e7165cee94977e5706ece0cb5e4
Manik <manik@pixelconception.com>
2023-07-05 13:12:55 +0530

    Update index.ts

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,97 +1,40 @@
-interface Button {
-  render(): void;
-  onClick(f: Function): void;
-}
-
-interface Checkbox {
-  render(): void;
-  toggle(): void;
-}
-
-interface GUIFactory {
-  createButton(): Button;
-  createCheckbox(button: Button): Checkbox;
-}
-
-class WindowsButton implements Button {
-  public render(): void {
-    console.log("Render a button in Windows Style");
-  }
-  public onClick(f: Function): void {
-    console.log("Windows button was clicked");
-    f();
+class Grinder {
+  public grindBeads(): void {
+    console.log("Grinding beans ...");
   }
 }
 
-class WindowsCheckbox implements Checkbox {
-  constructor(private button: Button) {}
-
-  public render(): void {
-    console.log("Render a checkbox in Windows Style");
-  }
-
-  toggle(): void {
-    this.button.onClick(() => {
-      console.log("Windows checkbox toggled");
-    });
+class Boiler {
+  public boilWater(): void {
+    console.log("Boiling Water ...");
   }
 }
 
-class MacOSButton implements Button {
-  public render(): void {
-    console.log("Render a button in MacOS Style");
-  }
-  public onClick(f: Function): void {
-    console.log("MacOS button was clicked");
-    f();
+class Brewer {
+  public brewCoffee(): void {
+    console.log("Brewing Coffee ...");
   }
 }
 
-class MacOSCheckbox implements Checkbox {
-  constructor(private button: Button) {}
+class CoffeeMakerFacade {
+  constructor(
+    private grinder: Grinder,
+    private boiler: Boiler,
+    private brewer: Brewer
+  ) {}
 
-  public render(): void {
-    console.log("Render a checkbox in MacOS Style");
-  }
-
-  toggle(): void {
-    this.button.onClick(() => {
-      console.log("MacOS checkbox toggled");
-    });
-  }
-}
-
-class WindowsFactory implements GUIFactory {
-  public createButton(): Button {
-    return new WindowsButton();
-  }
-  public createCheckbox(button: Button): Checkbox {
-    return new WindowsCheckbox(button);
+  public makeCoffee() {
+    this.grinder.grindBeads();
+    this.boiler.boilWater();
+    this.brewer.brewCoffee();
+    console.log("The Coffee is ready");
   }
 }
 
-class MacOsFactory implements GUIFactory {
-  public createButton(): Button {
-    return new MacOSButton();
-  }
-  public createCheckbox(button: Button): Checkbox {
-    return new MacOSCheckbox(button);
-  }
-}
+// Client code
+let grinder = new Grinder();
+let boilder = new Boiler();
+let brewer = new Brewer();
 
-// Client Code
-function renderUI(factory: GUIFactory) {
-  const button = factory.createButton();
-  const checkbox = factory.createCheckbox(button);
-
-  button.render();
-  checkbox.render();
-
-  button.onClick(() => {
-    console.log("Button Clicked");
-  });
-  checkbox.toggle();
-}
-
-renderUI(new WindowsFactory());
-renderUI(new MacOsFactory());
+let coffeeMaker = new CoffeeMakerFacade(grinder, boilder, brewer);
+coffeeMaker.makeCoffee();
