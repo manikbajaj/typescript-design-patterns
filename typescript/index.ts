77bfcc86a9fe5ce95a9ad052a11fcf72b40ef756
Manik <manik@pixelconception.com>
2023-07-07 10:37:33 +0530

    Update index.ts

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,74 +1,41 @@
-interface FileSystemComponent {
-  getName(): string;
-  getSize(): number;
+interface Coffee {
+  cost(): number;
+  description(): string;
 }
 
-class FileComponent implements FileSystemComponent {
-  constructor(private name: string, private size: number) {}
-
-  public getName(): string {
-    return this.name;
+class SimpleCoffee implements Coffee {
+  public cost(): number {
+    return 10;
   }
 
-  public getSize(): number {
-    return this.size;
-  }
-}
-
-interface CompositeFileSystemComponent extends FileSystemComponent {
-  addComponent(component: FileSystemComponent): void;
-  removeComponent(component: FileSystemComponent): void;
-  getComponents(): FileSystemComponent[];
-}
-
-class Folder implements CompositeFileSystemComponent {
-  private components: FileSystemComponent[] = [];
-  constructor(private name: string) {}
-
-  public getName(): string {
-    return this.name;
-  }
-
-  public getSize(): number {
-    return this.components.reduce(
-      (total, component) => total + component.getSize(),
-      0
-    );
-  }
-
-  public addComponent(component: FileSystemComponent): void {
-    this.components.push(component);
-  }
-
-  public removeComponent(component: FileSystemComponent): void {
-    const index = this.components.indexOf(component);
-    if (index !== -1) {
-      this.components.splice(index, 1);
-    }
-  }
-
-  public getComponents(): FileSystemComponent[] {
-    return this.components;
+  public description(): string {
+    return "Simple Coffee";
   }
 }
 
-// Client Code
-let file1 = new FileComponent("file1.txt", 500);
-let file2 = new FileComponent("file2.txt", 800);
-let file3 = new FileComponent("file3.txt", 1200);
+abstract class CoffeeDecorator implements Coffee {
+  constructor(protected coffee: Coffee) {}
 
-let folder = new Folder("My Folder");
-folder.addComponent(file1);
-folder.addComponent(file2);
-folder.addComponent(file3);
+  abstract cost(): number;
+  abstract description(): string;
+}
 
-console.log(`Folder ${folder.getName()}  Contains:`);
-folder
-  .getComponents()
-  .map((component) =>
-    console.log(
-      `- ${component.getName()} with the size of ${component.getSize()} bytes`
-    )
-  );
+class MilkDecorator extends CoffeeDecorator {
+  constructor(coffee: Coffee) {
+    super(coffee);
+  }
+  public cost(): number {
+    return this.coffee.cost() + 2;
+  }
 
-console.log(`Total Size ${folder.getSize()}`);
+  public description(): string {
+    return `${this.coffee.description()}, with milk`;
+  }
+}
+
+// client code
+let coffee: Coffee = new SimpleCoffee();
+coffee = new MilkDecorator(coffee);
+
+console.log(`Cost: ${coffee.cost()}`);
+console.log(`Description: ${coffee.description()}`);
