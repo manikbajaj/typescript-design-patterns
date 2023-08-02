066b967d355c40132c7f3cad6a37e5ca456645ab
Manik <manik@pixelconception.com>
2023-08-02 12:54:10 +0530

    Update index.ts

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,69 +1,91 @@
 interface Observer {
-  update(subject: Subject): void;
-}
-
-class ConcreteObserever implements Observer {
-  constructor(private id: number) {}
-  public update(subject: Subject): void {
-    console.log(
-      `Observer ${this.id} updated, new state: ${subject.getState()}`
-    );
-  }
+  update(temperature: number, humidity: number, pressure: number): void;
 }
 
 interface Subject {
-  addObserver(observer: Observer): void;
+  registerObserver(observer: Observer): void;
   removeObserver(observer: Observer): void;
   notifyObservers(): void;
-  getState(): number;
-  setState(state: number): void;
 }
 
-class ConcreteSubject implements Subject {
+class WeatherData implements Subject {
   private observers: Observer[] = [];
-  private state: number = 0;
+  private temperature: number | undefined;
+  private humidity: number | undefined;
+  private pressure: number | undefined;
 
-  public addObserver(observer: Observer): void {
-    const isExists = this.observers.includes(observer);
-    if (isExists) {
-      return console.log("Observer already exists");
-    }
-
+  public registerObserver(observer: Observer): void {
     this.observers.push(observer);
-    console.log("Observer Added Successfully");
   }
 
   public removeObserver(observer: Observer): void {
-    const observerIndex = this.observers.indexOf(observer);
-    if (observerIndex === -1) {
-      return console.log("Observer Does not Exist");
+    const index = this.observers.indexOf(observer);
+    if (index >= 0) {
+      this.observers.splice(index, 1);
     }
-    this.observers.splice(observerIndex, 1);
-    console.log("Observer was successfully removed");
   }
 
   public notifyObservers(): void {
-    this.observers.forEach((observer) => observer.update(this));
+    if (
+      this.temperature !== undefined &&
+      this.humidity !== undefined &&
+      this.pressure !== undefined
+    ) {
+      for (let observer of this.observers) {
+        observer.update(this.temperature, this.humidity, this.pressure);
+      }
+    }
   }
 
-  public getState(): number {
-    return this.state;
-  }
-
-  public setState(state: number): void {
-    this.state = state;
-    console.log("Setting State ....");
+  public setMeasurements(
+    temperature: number,
+    humidity: number,
+    pressure: number
+  ): void {
+    this.temperature = temperature;
+    this.humidity = humidity;
+    this.pressure = pressure;
     this.notifyObservers();
+  }
+
+  // additional weather data
+}
+
+class CurrentConditionsDisplay implements Observer {
+  private temperature: number | undefined;
+  private humidity: number | undefined;
+  private pressure: number | undefined;
+
+  constructor(private weatherData: Subject) {
+    this.weatherData.registerObserver(this);
+  }
+
+  public update(temperature: number, humidity: number, pressure: number): void {
+    this.temperature = temperature;
+    this.humidity = humidity;
+    this.pressure = pressure;
+    this.display();
+  }
+
+  public display(): void {
+    if (
+      this.temperature !== undefined &&
+      this.humidity !== undefined &&
+      this.pressure !== undefined
+    ) {
+      console.log(
+        `Temperature: ${this.temperature}, Humidity: ${this.humidity}, Pressure: ${this.pressure}`
+      );
+    } else {
+      console.log("Weather data is not available");
+    }
   }
 }
 
 // client code
-const subject = new ConcreteSubject();
+const weatherData = new WeatherData();
+const currentDisplay = new CurrentConditionsDisplay(weatherData);
 
-const observer1 = new ConcreteObserever(1);
-subject.addObserver(observer1);
-
-const observer2 = new ConcreteObserever(2);
-subject.addObserver(observer2);
-
-subject.setState(123);
+// Simulate new Weather Adjustments
+weatherData.setMeasurements(80, 65, 30.4);
+weatherData.setMeasurements(82, 70, 30.4);
