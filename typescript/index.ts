91aa48b7927a4e3ea83b49fdddf51c3d57599e72
Manik <manik@pixelconception.com>
2023-08-05 11:51:37 +0530

    Update index.ts

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,54 +1,49 @@
-abstract class CakeRecipe {
-  public bakeCake(): void {
-    this.preHeatOven();
-    this.mixIngredients();
-    this.bake();
-    this.coolingDown();
-    this.decorate();
+abstract class DataParser {
+  public parseData(): void {
+    this.loadData();
+    const data = "Sample Data";
+    const parsedData = this.parse(data);
+    this.validate(parsedData);
+    this.useData(parsedData);
   }
 
-  protected preHeatOven(): void {
-    console.log("Preheating oven to 175 Degree C");
+  protected loadData(): void {
+    console.log("Loading Data...");
   }
 
-  protected bake(): void {
-    console.log("Baking cake ....");
+  protected validate(data: any): void {
+    console.log("Validating data....");
   }
 
-  protected coolingDown(): void {
-    console.log("Cooling down the cake ...");
+  protected useData(data: any): void {
+    console.log("Using Data...");
   }
 
-  protected decorate(): void {
-    console.log("Decorating cake ...");
-  }
-
-  protected abstract mixIngredients(): void;
+  protected abstract parse(data: any): void;
 }
 
-class ChocolateCake extends CakeRecipe {
-  protected mixIngredients(): void {
-    console.log("Mixing: chocolate, sugar, butter, flour, eggs");
-  }
-
-  protected decorate(): void {
-    console.log(" Decorating cake with chocolate");
+class JSONParser extends DataParser {
+  protected parse(data: any): void {
+    console.log("Parsing data as JSON");
+    // JSON.parse(data);
+    return data;
   }
 }
 
-class VanillaCake extends CakeRecipe {
-  protected mixIngredients(): void {
-    console.log("Mixing: vanilla extract, sugar, butter, flour, eggs");
+class XMLParser extends DataParser {
+  protected parse(data: any): void {
+    console.log("Parsing data as XML");
+    return data;
   }
 }
 
-// client code
-function bakecake(cake: CakeRecipe) {
-  cake.bakeCake();
+// Client code
+function dataParser(dataParser: DataParser) {
+  dataParser.parseData();
 }
 
-console.log("Baking a chocolate cake");
-bakecake(new ChocolateCake());
+console.log("Parsing JSON data");
+dataParser(new JSONParser());
 
-console.log("Baking a vanilla cake");
-bakecake(new VanillaCake());
+console.log("Parsing XML data");
+dataParser(new XMLParser());
