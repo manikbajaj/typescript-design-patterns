852ad93debe433b37b0f02092afc1d1be29fb886
Manik <manik@pixelconception.com>
2023-08-09 08:10:54 +0530

    Update index.ts

--- typescript/index.ts
+++ typescript/index.ts
@@ -3,60 +3,25 @@ interface ICommand {
   undo(): void;
 }
 
-class Light {
-  public turnOn(): void {
-    console.log("The Light is on");
-  }
-
-  public turnOff(): void {
-    console.log("The Light is off");
-  }
-}
-
-class TurnOnCommand implements ICommand {
-  constructor(private light: Light) {}
-
-  public execute(): void {
-    this.light.turnOn();
-  }
-
-  public undo(): void {
-    this.light.turnOff();
-  }
-}
-
-class TurnOffCommand implements ICommand {
-  constructor(private light: Light) {}
-
-  public execute(): void {
-    this.light.turnOff();
-  }
-
-  public undo(): void {
-    this.light.turnOn();
-  }
-}
-
-class SimpleRemoteControl {
-  private currentCommand!: ICommand;
-  private undoCommand!: ICommand;
+class MyFileSystem {
   private commandQueue: ICommand[] = [];
 
-  public setCommand(command: ICommand): void {
-    this.undoCommand = this.currentCommand;
-    this.currentCommand = command;
+  public addCommand(command: ICommand): void {
     this.commandQueue.push(command);
   }
 
-  public buttonWasPressed(): void {
-    if (this.commandQueue.length) {
-      const command = this.commandQueue.shift();
+  public executeCommand(): void {
+    if (this.commandQueue.length > 0) {
+      let command = this.commandQueue.shift();
       command?.execute();
     }
   }
 
-  public undoButtonWasPressed(): void {
-    this.undoCommand.execute();
+  public undoCommand(): void {
+    if (this.commandQueue.length > 0) {
+      let command = this.commandQueue.pop();
+      command?.undo();
+    }
   }
 
   public hasCommands(): boolean {
@@ -64,25 +29,83 @@ class SimpleRemoteControl {
   }
 }
 
-// client Code
-const remote: SimpleRemoteControl = new SimpleRemoteControl();
-const light: Light = new Light();
+class CreateFileCommand implements ICommand {
+  constructor(private path: string) {}
 
-// Turning On The Light
-remote.setCommand(new TurnOnCommand(light));
-remote.buttonWasPressed();
+  public execute(): void {
+    console.log(`Creating file at ${this.path}`);
+    // logic
+  }
 
-// Turning Off the Light
-remote.setCommand(new TurnOffCommand(light));
-remote.buttonWasPressed();
-
-// Undo last operation
-remote.undoButtonWasPressed();
-
-// Create a command que
-remote.setCommand(new TurnOnCommand(light));
-remote.setCommand(new TurnOffCommand(light));
-
-while (remote.hasCommands()) {
-  remote.buttonWasPressed();
+  public undo(): void {
+    console.log(`Deleting file at ${this.path}`);
+  }
 }
+
+class DeleteFileCommand implements ICommand {
+  constructor(private path: string) {}
+
+  public execute(): void {
+    console.log(`Deleting file at ${this.path}`);
+    // logic
+  }
+
+  public undo(): void {
+    console.log(`Restoring file at ${this.path}`);
+  }
+}
+
+class ReadFileCommand implements ICommand {
+  constructor(private path: string) {}
+
+  public execute(): void {
+    console.log(`Reading file at ${this.path}`);
+    // logic
+  }
+
+  public undo(): void {
+    console.log(`undo operation not available`);
+  }
+}
+
+class UpdateFileCommand implements ICommand {
+  constructor(
+    private path: string,
+    private newContent: string,
+    private oldContent: string
+  ) {}
+
+  public execute(): void {
+    console.log(`Updating file ${this.path}, new content: ${this.newContent}`);
+  }
+
+  public undo(): void {
+    console.log(`Reverting file ${this.path}, old content: ${this.oldContent}`);
+  }
+}
+
+// client code
+let myFileSystem = new MyFileSystem();
+
+// creating a file
+myFileSystem.addCommand(new CreateFileCommand("/path/file.txt"));
+
+// Update file
+let updateFile = new UpdateFileCommand(
+  "/path/file.txt",
+  "new content",
+  "old content"
+);
+myFileSystem.addCommand(updateFile);
+
+// Read file
+myFileSystem.addCommand(new ReadFileCommand("/path/file.txt"));
+
+// deleting a command
+myFileSystem.addCommand(new DeleteFileCommand("/path/file.txt"));
+
+while (myFileSystem.hasCommands()) {
+  myFileSystem.executeCommand();
+}
+
+myFileSystem.undoCommand();
