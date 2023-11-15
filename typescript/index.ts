17c9ae917835b9db0ce1fe9df2aa33a8ed0c42da
Manik <manik@pixelconception.com>
2023-11-15 18:49:38 +0530

    Update index.ts

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,16 +1,34 @@
-class Bird {
-  fly(): void {
-    console.log("Bird is flying");
+abstract class Shape {
+  abstract calculateArea(): number;
+}
+
+class Rectangle extends Shape {
+  constructor(public width: number, public height: number) {
+    super();
+  }
+
+  public calculateArea(): number {
+    return this.width * this.height;
   }
 }
 
-class FlightlessBird extends Bird {
-  fly(): void {
-    console.log("Flightless Birds Cannot Fly");
+class Square extends Shape {
+  constructor(public side: number) {
+    super();
+  }
+
+  public calculateArea(): number {
+    return this.side * this.side;
   }
 }
 
-class Penguin extends FlightlessBird {}
+// ====== Client Code
+function area(shape: Shape) {
+  return shape.calculateArea();
+}
 
-let penguin: Penguin = new Penguin();
-console.log(penguin.fly());
+let rectangle = new Rectangle(10, 12);
+let square = new Square(8);
+
+area(rectangle); // 120
+area(square); // 64
